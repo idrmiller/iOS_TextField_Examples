985ff4a7ac57deaf32ea997567678da0e440d954
idrmiller <idrmiller360@gmail.com>
2016-01-25 21:49:04 -0500

    Initial Commit

---
+++ iOS_TextField_Delgates/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOS_TextField_Delgates
+//
+//  Created by David Miller on 1/25/16.
+//  Copyright © 2016 LM Labs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ iOS_TextField_DelgatesTests/iOS_TextField_DelgatesTests.swift
@@ -0,0 +1,36 @@
+//
+//  iOS_TextField_DelgatesTests.swift
+//  iOS_TextField_DelgatesTests
+//
+//  Created by David Miller on 1/25/16.
+//  Copyright © 2016 LM Labs. All rights reserved.
+//
+
+import XCTest
+@testable import iOS_TextField_Delgates
+
+class iOS_TextField_DelgatesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
